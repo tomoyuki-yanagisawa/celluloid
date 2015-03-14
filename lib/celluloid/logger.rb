349a6ecc6ec708f6ec280c92e253bfdc43ae8ace
Tim Carey-Smith <tim@spork.in>
2015-03-13 17:45:26 -0700

    Do not pull out the detail for exceptions

--- lib/celluloid/logger.rb
+++ lib/celluloid/logger.rb
@@ -85,7 +85,7 @@ module Celluloid
 
     # Format an exception message
     def format_exception(exception)
-      str = "#{exception.class}: #{exception.to_s}\n\t"
+      str = "#{exception.class}\n\t"
       if exception.backtrace
         str << exception.backtrace.join("\n\t")
       else
